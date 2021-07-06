a28673bc8971f26e2f6945a5c97a374ae93800b0
starlordamit <59335027+starlordamit@users.noreply.github.com>
2021-07-06 12:33:19 +0530

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,22 +1,12 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
-from flask import Flask
+from flask import Flask 
+from flask_autoindex import AutoIndex
 
-# Flask constructor takes the name of
-# current module (__name__) as argument.
 app = Flask(__name__)
 
-# The route() function of the Flask class is a decorator,
-# which tells the application which URL should call
-# the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-	return 'Hello World'
+ppath = "/" # update your own parent directory here
 
-# main driver function
-if __name__ == '__main__':
+app = Flask(__name__)
+AutoIndex(app, browse_root=ppath)    
 
-	# run() method of Flask class runs the application
-	# on the local development server.
-	app.run(host='0.0.0.0', port=8080)
+if __name__ == "__main__":
+    app.run()
